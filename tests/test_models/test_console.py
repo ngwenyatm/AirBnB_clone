26c9c2e6ba3d3fdcfd0edd4ef52328d1d2341327
ngwenyatm <thabsnspaces@gmail.com>
2024-03-20 13:19:39 -0700

    pushed with script

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -2,23 +2,23 @@ import unittest
 from console import HBNBCommand
 
 class TestHBNBCommand(unittest.TestCase):
-
+# test case units for methods within the custom console
     def test_prompt(self):
-        """Test the custom prompt."""
+        """Test custom prompt."""
         self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
 
     def test_do_quit(self):
-        """Test the quit command."""
+        """Test quit command."""
         interpreter = HBNBCommand()
         self.assertTrue(interpreter.do_quit(""))
 
     def test_do_EOF(self):
-        """Test the EOF handling."""
+        """Test EOF handling."""
         interpreter = HBNBCommand()
         self.assertTrue(interpreter.do_EOF(""))
 
     def test_emptyline(self):
-        """Test empty line behavior."""
+        """Test line behavior."""
         interpreter = HBNBCommand()
         # No assertion needed, emptyline should do nothing
         interpreter.emptyline()
