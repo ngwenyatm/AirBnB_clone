b17aa5de7dae469d5f8f70ad0d2e5fd4a8f2bf2a
MakamuT <makamuthe2@gmail.com>
2024-03-22 16:21:00 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 from datetime import datetime
+from models.__init__ import storage
 import uuid
 import json
 
-"""defines all common attributes/methods for other classes"""
-
 class BaseModel:
-    """
-    if kwargs is not empty:
-        each key of this dictionary is an attribute name (Note __class__ from kwargs is the only one that should not be added as an attribute).
-        each value of this dictionary is the value of this attribute name
-        created_at and updated_at are strings in this dictionary, but inside your BaseModel instance is working with datetime object.
-    """
-
+    """defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == '__class__':
                     continue
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT:%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT:%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = self.created_at
-                    storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     """returns object as a str"""
     def __str__(self):
