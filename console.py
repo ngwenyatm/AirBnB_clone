43b19bd0a6aedb6280bde82c773f73246c80446d
ngwenyatm <thabsnspaces@gmail.com>
2024-03-23 08:50:03 -0700

    checkertest console1

--- console.py
+++ console.py
@@ -1,66 +1,24 @@
 #!/usr/bin/python3
 
 import cmd
-import json
 
 class HBNBCommand(cmd.Cmd):
+    """HBNB command interpreter."""
 
     prompt = "(hbnb) "
 
+    def do_quit(self, args):
+        """Quit the command interpreter."""
+        return True
+
+    def do_EOF(self, args):
+        """Exit on end-of-file (Ctrl-D)."""
+        print()
+        return True
+
     def emptyline(self):
-       " ""do nothing"""
+        """Do nothing on empty lines."""
         pass
 
-    def do_quit(self, line):
-        """exits the program"""
-        return True
-
-    def do_EOF(self, line):
-        """handles EOF"""
-        return True
-
-    def do_help(self, line):
-        """keep this updated"""
-        pass
-   """ 
-    def do_create(self, line):
-        tokens = line.split()
-        
-        if len(tokens) < 2:
-            print("** class name missing **")
-            return
-        elif line not in storage.classes():
-            print("** class doesn't exist **")
-            return
-        else:
-            model_class = models.BaseModel
-            all_objects = storage.all()
-            new_id = 1
-
-        while str(new_id) in all_objects:
-            new_id += 1
-
-        new_instance = model_class(id=new_id)
-        storage.save()
-        print(new_instance.id)
-
-    def do_show(self,line):
-        args = line.split()
-        
-        if len(args) < 2:
-            print("** class name missing **")
-        elif args[0] not in storage.classes():
-            print("** class doesn't exist **")
-        elif len(args) < 3:
-            print("** instance id missing **")
-        else:
-            class_name = args[0]
-            instance_id = args[2]
-            try:
-                instance = storage.get(class_name, instance_id)
-                print(instance)
-            except KeyError:
-                print("** no instance found **")
-                """
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
