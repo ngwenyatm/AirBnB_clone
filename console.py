e287e359fbfee20cff50c3b13489398e496c2d07
ngwenyatm <thabsnspaces@gmail.com>
2024-03-23 08:22:26 -0700

    checkertest console1

---
+++ console.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+import cmd
+import json
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, line):
+        return True
+
+    def do_EOF(self, line):
+        return True
+
+    def do_help(self, line):
+        pass
+    
+    def do_create(self, line):
+        tokens = line.split()
+        
+        if len(tokens) < 2:
+            print("** class name missing **")
+            return
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+            return
+        else:
+            model_class = models.BaseModel
+            all_objects = storage.all()
+            new_id = 1
+
+        while str(new_id) in all_objects:
+            new_id += 1
+
+        new_instance = model_class(id=new_id)
+        storage.save()
+        print(new_instance.id)
+
+    def do_show(self,line):
+        args = line.split()
+        
+        if len(args) < 2:
+            print("** class name missing **")
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) < 3:
+            print("** instance id missing **")
+        else:
+            class_name = args[0]
+            instance_id = args[1]
+            try:
+                instance = storage.get(class_name, instance_id)
+                print(instance)
+            except KeyError:
+                print("** no instance found **")
+
+    def default(self, line):
+        print(f"unknown function:{line}")
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
