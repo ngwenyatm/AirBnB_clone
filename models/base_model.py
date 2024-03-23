a7a69c8afd5209f9fa110b9c33979c2958b9c794
MakamuT <makamuthe2@gmail.com>
2024-03-23 13:55:00 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,7 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key,
-                            datetime.strptime(value, "%Y-%m-%dT:%H:%M:%S.%f"))
+                    setattr(self, key,datetime.strptime(value, "%Y-%m-%dT:%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
         else:
