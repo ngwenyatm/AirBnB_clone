ad8676b73041943b866c057cb0825a961a775f51
ngwenyatm <thabsnspaces@gmail.com>
2024-03-24 09:00:28 -0700

    ..

--- console.py
+++ console.py
@@ -19,34 +19,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         tokens = line.split()
-        if line == "" or line is None: 
+        if line == "" or line is None:
             print("** class name missing **")
             return
         elif tokens[0] != "BaseModel":
             print("** class doesn't exist **")
 
-            obj = BaseModel()
+            obj = eval(tokens[0])()
             obj.save
             print(obj.id)
-
-    def do_destroy(self, line):
-        tokens = line.split()
-        if len(tokens) == 0:
-            print("** class name missing **")
-            return
-        elif len(tokens) < 2:
-            print("** instance id missing **")
-            return
-        elif tokens[0] != "BaseModel":
-            print("** class doesn't exist **")
-        else:
-            obj = storage.all()
-            key = "{}.{}".format(tokens[0], tokens[1])
-            if key in obj:
-                del obj[key]
-                storage.save()
-            else:
-                print("** no instance found **")
 
     def do_show(self, line):
 
@@ -79,8 +60,27 @@ class HBNBCommand(cmd.Cmd):
                       if type(obj).__name__ == args[0]]
                 print(n_list)
         else:
-            new = [str(obj) for key, obj in storage.all()]
-            print(new)
+            new_list = [str(obj) for key, obj in storage.all()]
+            print(new_list)
 
-    if __name__ == "__main__":
+    def do_destroy(self, line):
+        tokens = line.split()
+        if len(tokens) == 0:
+            print("** class name missing **")
+            return
+        elif len(tokens) < 2:
+            print("** instance id missing **")
+            return
+        elif tokens[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = storage.all()
+            key = "{}.{}".format(tokens[0], tokens[1])
+            if key in obj:
+                del obj[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
