c447f30f9ecf64d1c8bc4b6db5d706b13fec939d
MakamuT <makamuthe2@gmail.com>
2024-03-25 13:00:32 -0700

    airbnb

--- models/user.py
+++ models/user.py
@@ -4,6 +4,11 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, email="", password="", first_name="",
                  last_name="", *args, **kwargs):
         super().__init__(*args, **kwargs)
